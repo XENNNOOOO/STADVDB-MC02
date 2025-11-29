a6f8ab0dd8bd4df24bf5fbfa525db8aebcf6e886
leebrien <lee_sandoval@dlsu.edu.ph>
2025-11-29 22:49:28 +0800

    test: different host

--- stadvdb-mco2/lib/connections.ts
+++ stadvdb-mco2/lib/connections.ts
@@ -5,7 +5,7 @@ import type { NodeName } from './types';
 
 // Node 0 (Server 0) - Master, All Data
 const node0_Central_Config: mysql.ConnectionOptions = {
-  host: '127.0.0.1', // localhost via SSH tunnel
+  host: '10.2.14.135', // localhost via SSH tunnel
   port: 3306, // tunneled from Server0
   user: 'app_user',
   password: 'xACmJk5u4QNK3ESHvW7XrpFg@123',
@@ -15,7 +15,7 @@ const node0_Central_Config: mysql.ConnectionOptions = {
 
 // Node 1 (Server 1) - 2025 (Primary) + 2024 (Backup)
 const node1_Config: mysql.ConnectionOptions = {
-  host: '127.0.0.1', // localhost via SSH tunnel
+  host: '10.2.14.136', // localhost via SSH tunnel
   port: 3307, // tunneled from Server1
   user: 'app_user',
   password: 'xACmJk5u4QNK3ESHvW7XrpFg@123',
@@ -25,7 +25,7 @@ const node1_Config: mysql.ConnectionOptions = {
 
 // Node 2 (Server 2) - 2024 (Primary) + 2025 (Backup)
 const node2_Config: mysql.ConnectionOptions = {
-  host: '127.0.0.1', // localhost via SSH tunnel
+  host: '10.2.14.137', // localhost via SSH tunnel
   port: 3308, // tunneled from Server2
   user: 'app_user',
   password: 'xACmJk5u4QNK3ESHvW7XrpFg@123',
