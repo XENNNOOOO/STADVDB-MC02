29d830e8d55b427047c6621e85d10e987bc55865
leebrien <lee_sandoval@dlsu.edu.ph>
2025-11-29 23:33:30 +0800

    fix: removed ui dropdown arrow

--- stadvdb-mco2/app/components/OrderList.tsx
+++ stadvdb-mco2/app/components/OrderList.tsx
@@ -177,9 +177,7 @@ export default function OrderList() {
             <h2 className="text-lg font-semibold text-slate-800">Orders</h2>
             <div className="relative">
               <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-3">
-                <svg className="h-5 w-5 text-slate-500" viewBox="0 0 20 20" fill="currentColor">
-                  <path fillRule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clipRule="evenodd" />
-                </svg>
+
               </div>
             </div>
           </div>
