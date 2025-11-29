5880b2e4b58cae7a49d29672bcd9a6cfbbcef58d
leebrien <lee_sandoval@dlsu.edu.ph>
2025-11-29 17:28:11 +0800

    fix: connections for deployment

--- stadvdb-mco2/lib/connections.ts
+++ stadvdb-mco2/lib/connections.ts
@@ -1,44 +1,39 @@
 import mysql from 'mysql2/promise';
 import type { NodeName } from './types';
 
-// Connection Configs
+const baseConfig = {
+  user: process.env.DB_USER || 'app_user',
+  password: process.env.DB_PASSWORD, 
+  connectTimeout: 5000,
+  ssl: { rejectUnauthorized: false }
+};
 
-// Node 0 (Server 0) - Master, All Data
+// Node 0 
 const node0_Central_Config: mysql.ConnectionOptions = {
-  host: '127.0.0.1', // localhost via SSH tunnel
-  port: 3306, // tunneled from Server0
-  user: 'app_user',
-  password: 'xACmJk5u4QNK3ESHvW7XrpFg@123',
-  database: 'node0',
-  connectTimeout: 3000,
+  ...baseConfig,
+  host: process.env.NODE0_HOST,
+  port: Number(process.env.NODE0_PORT),
+  database: process.env.NODE0_DB,
 };
 
-// Node 1 (Server 1) - 2025 (Primary) + 2024 (Backup)
+// Node 1
 const node1_Config: mysql.ConnectionOptions = {
-  host: '127.0.0.1', // localhost via SSH tunnel
-  port: 3307, // tunneled from Server1
-  user: 'app_user',
-  password: 'xACmJk5u4QNK3ESHvW7XrpFg@123',
-  database: 'node1',
-  connectTimeout: 3000,
+  ...baseConfig,
+  host: process.env.NODE1_HOST,
+  port: Number(process.env.NODE1_PORT),
+  database: process.env.NODE1_DB,
 };
 
-// Node 2 (Server 2) - 2024 (Primary) + 2025 (Backup)
+// Node 2
 const node2_Config: mysql.ConnectionOptions = {
-  host: '127.0.0.1', // localhost via SSH tunnel
-  port: 3308, // tunneled from Server2
-  user: 'app_user',
-  password: 'xACmJk5u4QNK3ESHvW7XrpFg@123',
-  database: 'node2',
-  connectTimeout: 3000,
+  ...baseConfig,
+  host: process.env.NODE2_HOST,
+  port: Number(process.env.NODE2_PORT),
+  database: process.env.NODE2_DB,
 };
 
-/**
- * Creates a single, one-time connection to the specified node.
- * We use createConnection (not a pool) to make it possible
- * to catch and demonstrate connection failures for MCO2.
- */
-export const getConnection = (node: NodeName) => {
+export const getConnection = async (node: NodeName) => {
+
   if (node === 'node1') {
     return mysql.createConnection(node1_Config);
   }
